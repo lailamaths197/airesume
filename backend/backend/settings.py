ee4626d0fa1c3851dd97c839b6b52a81b10743ce
lailamaths197 <lailamaths197@gmail.com>
2026-07-18 06:39:16 +0530

    fix production cors handshake alignments

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,7 +57,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://onrender.com"
+    "https://onrender.com",
 ]
 
 STATIC_URL = 'static/'
